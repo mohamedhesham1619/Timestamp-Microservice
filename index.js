cfe872dee5f354f5b38804dc813a6629792d7d7c
Mohamed Hesham <mohamedhesham1619@gmail.com>
2022-09-24 18:50:14 +0200

    feat: add '/api/:date' endpoint

--- index.js
+++ index.js
@@ -20,8 +20,18 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+  let date = new Date(req.params.date)
+
+  if (date == 'Invalid Date'){
+    res.json({error: 'Invalid Date'})
+  }
+  else{
+    res.json({
+      unix: date.valueOf(),
+      utc: date.toUTCString()
+    })
+  }
 });
 
 
